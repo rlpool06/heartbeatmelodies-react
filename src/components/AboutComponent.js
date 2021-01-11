b175a60ed58bcffbe1fd6dc873f3c8109059444b
Ryan Pool <rlpool06@gmail.com>
2021-01-11 12:09:30 -0600

    Carousel Update

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Jumbotron, Card, CardBody, Row, Col, Container } from 'reactstrap';
 import Carousel from './CarouselComponent.js';
 
 class About extends Component {
@@ -8,12 +8,46 @@ class About extends Component {
         <div className="bgImg">
             <React.Fragment>
                 <Jumbotron fluid>
-                        <div className="container-fluid d-flex justify-content-center mt-auto">
+                        <div className="d-flex justify-content-center mt-auto">
                             <img src="fwdlogos/Heartbeat Melodies WHT.png" alt="Heartbeat Melodies" width= "50%" />
                         </div>
                 </Jumbotron>
 
-                <Carousel />
+                <Container>
+                    <Card className="card-body-background2">
+                        <CardBody>
+                            <Row>
+                                <Col lg={6}>
+                                    <h3>Our Story</h3>
+                                    <p>The story of Heartbeat Melodies begins with a gift.  In 2018, my wife and I were awaiting the arrival of our baby girl.  This was to be our first child so everything we could find to commemorate this new season found it's way into our shopping cart.  Baby gadgets, toys, cute clothes, photo albums, everything started piling up as we celebrated this new baby girl.</p> 
+                                    <p>Knowing my wife's desire to capture the excitement of pregnancy, I wanted to make something truly unique.  I started thinking through some of the most memorable moments we had experienced and hearing our daughter’s heartbeat for the first time was truly unforgettable.  Using my background in recording and music production, I decided to take an audio clip of my daughter’s heartbeat and create a short song as a gift for my wife.</p>
+                                </Col>
+                                <Col lg={6}>
+                                    <img src='./img/ourstory.jpg' alt="Our story" className="img-fluid" />
+                                </Col>
+                            </Row>
+                            <Row className="mt-2">
+                                <Col>
+                                    <p>Of course, after both of us listened together, we were in tears.  A few weeks later, she played the song at her baby shower and it evoked the same reaction from everyone in the room.  Soon after, we started getting asked by friends if I did this for anyone else.  I started writing more songs for friends, and even a song for our niece.  Through writing these songs, the idea for Heartbeat Melodies was born.</p>
+                                    <p>I’ve always had a passion for creating and playing music.  I also believe music has the capability of reminding us of special moments in our lives, taking us back to those emotions, feelings, and memories.  Through Heartbeat Melodies,   I hope to provide a unique experience for expecting parents that captures the joy and excitement of new life.</p>
+                                </Col>
+                            </Row>
+                        </CardBody>
+                    </Card>
+
+                    <Card className="card-body-background2 mt-5" >
+                        <CardBody className="text-center justify-content-center">
+                            <h2 className="row-content">Testimonials</h2>
+                            <p>See what people are saying about us!</p>
+                        </CardBody>
+                    </Card>
+
+                    <Carousel className="overlay" />
+
+                    
+                </Container>
+
+                
             
             </React.Fragment>
 
--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import { UncontrolledCarousel, Row, Col } from 'reactstrap';
 
 const TESTIMONIES = [
     {
-        src: '/img/mcphersons.jpg',
+        src: './img/mcphersons.jpg',
+        header: '',
         caption: "We are so thankful for the beautiful song Ryan created with our son’s heartbeat. He captured the prayers we had for Leon’s life using his musical talent and turned it into something we will cherish forever.",
         key: '1'
     },
     {
-        src: '/img/hootons.jpeg',
+        src: './img/hootons.jpeg',
+        header: '',
         caption: "This song is so special to our family! Ryan asked detailed questions in order to make ours super personalized to our son Miller. Still get emotional listening to it even now as he’s almost a year, remembering hearing it for the first time in such a weird season of waiting for him to arrive. It was seriously a gift, and one we can cherish for a long time!",
         key: '2'
     },
     {
-        src: '/img/berlangas.JPG',
+        src: './img/berlangas.JPG',
+        header: "The songs created for our two little girls by Heartbeat Melodies will be cherished for a lifetime, and not just by us, but by grandparents, siblings, and even our girls as they grow older. Such a sweet way to be able to continuously listen to their little heartbeats over and over again and remember the blessing these girls are, by having it put to a song uniquely written for each one of them. I could not recommend Heartbeat Melodies enough!",
         caption: "The songs created for our two little girls by Heartbeat Melodies will be cherished for a lifetime, and not just by us, but by grandparents, siblings, and even our girls as they grow older. Such a sweet way to be able to continuously listen to their little heartbeats over and over again and remember the blessing these girls are, by having it put to a song uniquely written for each one of them. I could not recommend Heartbeat Melodies enough!",
         key: '3'
     }
 ];
 
-const Carousel = () => <UncontrolledCarousel items={TESTIMONIES} />
+const Carousel = () => (
+    <Row className='mt-5 mb-5'>
+        <Col md={8} className="mx-auto">
+            <UncontrolledCarousel items={TESTIMONIES} />
+        </Col>
+    </Row>
+);
+
 
 export default Carousel;
